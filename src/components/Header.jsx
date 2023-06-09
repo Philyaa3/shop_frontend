08c643b0114841cf13caa029a46959b25160f449
Philyaa3 <psychevskiy@gmail.com>
2023-06-09 17:50:39 +0300

    321 commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
+import Category from "./homeComponents/Category";
 
 const Header = ({setVisible, cartEnable}) => {
     const [cartCount, setCartCount] = useState(JSON.parse(localStorage.getItem("storage")).length);
-    const [isOpen, setIsOpen] = useState(false);
-
-    const toggleDropdown = () => {
-        setIsOpen(!isOpen);
-    };
 
     useEffect(() => {
         let buff = 0;
@@ -115,95 +111,7 @@ const Header = ({setVisible, cartEnable}) => {
                                         src="/images/logo.png"
                                     />
                                 </Link>
-                                <div className='col-md-3 category-button'>
-                                    <div className='overlay'
-                                         style={isOpen ? {display: 'block'} : {display: 'none'}}
-                                         onClick={toggleDropdown}
-                                    ></div>
-                                    <button className="toggle-button btn btn-dark" onClick={toggleDropdown}>
-                                        Categories
-                                    </button>
-                                    {isOpen &&
-                                        <div className="dropdown-content">
-                                            <ul className='list-group border-0 rounded-0 nav-list scroll w-30'>
-                                                <li className='list-group-item border-0'><Link to='/'>Красота и
-                                                    здоровье</Link></li>
-                                                <li className='list-group-item border-0'><Link to='/'>Дом и сад</Link>
-                                                </li>
-                                                <li className='list-group-item border-0'><Link to='/'>Одежда и
-                                                    обувь</Link></li>
-                                                <li className='list-group-item border-0'><Link to='/'>Техника и
-                                                    электроника</Link>
-                                                </li>
-                                                <li className='list-group-item border-0'><Link to='/'>Товары для
-                                                    детей</Link></li>
-                                                <li className='list-group-item border-0'><Link to='/'>Авто-, мото</Link>
-                                                </li>
-                                                <li className='list-group-item border-0'><Link to='/'>Подарки, хобби,
-                                                    книги</Link>
-                                                </li>
-                                                <li className='list-group-item border-0'><Link to='/'>Аксессуары и
-                                                    украшения</Link>
-                                                </li>
-                                                <li className='list-group-item border-0'><Link to='/'>Аксессуары и
-                                                    украшения</Link>
-                                                </li>
-                                                <li className='list-group-item border-0'><Link to='/'>Аксессуары и
-                                                    украшения</Link>
-                                                </li>
-                                            </ul>
-                                            <div className='display-flex flex-wrap gap-5 subMenu_Wrapper scroll'>
-                                                <ul className='subMenu'>
-                                                    <li>Косметика по догляду</li>
-                                                    <li>Догляд за обличчям</li>
-                                                    <li>Догляд за волоссям</li>
-                                                    <li>Догляд за тілом</li>
-                                                </ul>
-                                                <ul className='subMenu'>
-                                                    <li>Косметика по догляду</li>
-                                                    <li>Догляд за обличчям</li>
-                                                    <li>Догляд за волоссям</li>
-                                                    <li>Догляд за тілом</li>
-                                                </ul>
-                                                <ul className='subMenu'>
-                                                    <li>Косметика по догляду</li>
-                                                    <li>Догляд за обличчям</li>
-                                                    <li>Догляд за волоссям</li>
-                                                    <li>Догляд за тілом</li>
-                                                </ul>
-                                                <ul className='subMenu'>
-                                                    <li>Косметика по догляду</li>
-                                                    <li>Догляд за обличчям</li>
-                                                    <li>Догляд за волоссям</li>
-                                                    <li>Догляд за тілом</li>
-                                                </ul>
-                                                <ul className='subMenu'>
-                                                    <li>Косметика по догляду</li>
-                                                    <li>Догляд за обличчям</li>
-                                                    <li>Догляд за волоссям</li>
-                                                    <li>Догляд за тілом</li>
-                                                </ul>
-                                                <ul className='subMenu'>
-                                                    <li>Косметика по догляду</li>
-                                                    <li>Догляд за обличчям</li>
-                                                    <li>Догляд за волоссям</li>
-                                                    <li>Догляд за тілом</li>
-                                                </ul>
-                                                <ul className='subMenu'>
-                                                    <li>Косметика по догляду</li>
-                                                    <li>Догляд за обличчям</li>
-                                                    <li>Догляд за волоссям</li>
-                                                    <li>Догляд за тілом</li>
-                                                </ul>
-                                                <ul className='subMenu'>
-                                                    <li>Косметика по догляду</li>
-                                                    <li>Догляд за обличчям</li>
-                                                    <li>Догляд за волоссям</li>
-                                                    <li>Догляд за тілом</li>
-                                                </ul>
-                                            </div>
-                                        </div>}
-                                </div>
+                                <Category/>
                             </div>
                             <div className="col-md-3 d-flex align-items-center justify-content-end Login-Register">
                                 <div className="btn-group">
